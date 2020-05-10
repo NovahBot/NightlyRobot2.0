42ade88d007114b974ebb8e132df6e7678a166cc
santo-surviver <62856832+santo-surviver@users.noreply.github.com>
2020-05-11 05:05:04 +0530

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -62,15 +62,15 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     elif calc == 2:
         pairs.append((modules[-1], ))
 
-    max_num_pages = ceil(len(pairs) / 21)
+    max_num_pages = ceil(len(pairs) / 7)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
-        pairs = pairs[modulo_page * 21:21 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("⏹", callback_data="bot_start"),
-             EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("🧀", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🌏", callback_data="bot_start"),
+             EqInlineKeyboardButton("🍕", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
