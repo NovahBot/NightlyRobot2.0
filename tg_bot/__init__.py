6721adb19bfa39cab598e0577f38e97b9e7fa167
santo-surviver <62856832+santo-surviver@users.noreply.github.com>
2020-05-11 02:54:20 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -148,7 +148,6 @@ DEV_USERS.add(988452336)
 
 updater = tg.Updater(TOKEN, workers=WORKERS)
 dispatcher = updater.dispatcher
-tbot = TelegramClient("tg_bot", API_KEY, API_HASH)
 
 SUDO_USERS = list(SUDO_USERS) + list(DEV_USERS)
 DEV_USERS = list(DEV_USERS)
