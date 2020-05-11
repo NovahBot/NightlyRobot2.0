425199e4e683d86d6c51641229c2ae577eca90c5
santo-surviver <62856832+santo-surviver@users.noreply.github.com>
2020-05-11 23:58:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -246,7 +246,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="Help",url="t.me/{}?start=help".format(bot.username)),
-                                                InlineKeyboardButton(text="👥 Support chat.",url="https://telegram.dog/allukatm")]]))
+                                                InlineKeyboardButton(text="💫Channel",url="https://telegram.dog/imperialxxxx")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
